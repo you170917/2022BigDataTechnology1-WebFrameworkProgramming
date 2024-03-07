1fbb672b0bf03721573f7c6b8e141d039a14ca6f
qinyalan <xialan3315694298@qq.com>
2024-03-07 19:49:55 +0800

    覃亚兰的第一次提交

---
+++ 覃亚兰/homework01/src/test/java/org/example/homework01/Homework01ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.homework01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
