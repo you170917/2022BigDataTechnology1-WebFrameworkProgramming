4b4f455c49777bae046674f1fe0258272145ecb4
ZhouMeiNing <2893039946@qq.com>
2024-03-08 18:09:31 +0800

    周美宁的第二次提交

---
+++ 周美宁/homework03/src/main/java/com/example/homework03/Homework03Application.java
@@ -0,0 +1,13 @@
+package com.example.homework03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Homework03Application.class, args);
+	}
+
+}
