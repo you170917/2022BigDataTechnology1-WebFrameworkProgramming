4a0cdd0e5681b5d616e123e32e3e16653d0c54e3
WenJiuyangg <2990182265@qq.com>
2024-03-13 11:26:01 +0800

    文久杨的第一次提交

---
+++ 文久杨/YLW/src/main/java/com/example/ylw/YlwApplication.java
@@ -0,0 +1,13 @@
+package com.example.ylw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YlwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YlwApplication.class, args);
+    }
+
+}
