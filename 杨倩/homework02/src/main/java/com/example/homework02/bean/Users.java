8e0f0d59049959aa4064c7c145200c25b7f989c3
YangQian <2547898936@qq.com>
2024-03-15 00:55:44 +0800

    杨倩的第一次提交

---
+++ 杨倩/homework02/src/main/java/com/example/homework02/bean/Users.java
@@ -0,0 +1,53 @@
+package com.example.homework02.bean;
+
+import lombok.Data;
+
+@Data
+public class Users {
+    private Integer id;
+    private String name;
+    private Integer age;
+    private String gender;
+
+    public Users(Integer id, String name, Integer age, String gender) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    public Users() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
