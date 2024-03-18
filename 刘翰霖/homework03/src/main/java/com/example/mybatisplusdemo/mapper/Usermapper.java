52b6f50d14a6e89ecd61c9ef13488fd3d6081e09
24876021 <18282648320@163.com>
2024-03-18 19:12:48 +0800

    刘翰霖的第三次提交

---
+++ 刘翰霖/homework03/src/main/java/com/example/mybatisplusdemo/mapper/Usermapper.java
@@ -0,0 +1,7 @@
+package com.example.mybatisplusdemo.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.mybatisplusdemo.pojo.Users;
+
+public interface Usermapper extends BaseMapper<Users> {
+}
