06a006e3fdf0b2b39dc4f70c6ba1a9af8b7cba66
= <=>
2024-03-21 09:01:51 +0800

    李佳洁的第三次提交

---
+++ 李佳洁/homework03/src/main/java/com/example/homework03/bean/User.java
@@ -0,0 +1,15 @@
+package com.example.homework03.bean;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+@Data
+public class User {
+    @TableId(value = "id", type = IdType.AUTO)
+    private Integer id;
+    private String username;
+    private String address;
+
+}
