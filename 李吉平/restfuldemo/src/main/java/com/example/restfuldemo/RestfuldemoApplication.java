a33cd6f8d461868aff7092e238a3628470f69c21
Lory <2847644231@qq.com>
2024-04-01 11:14:11 +0800

    李吉平的第四次提交

---
+++ 李吉平/restfuldemo/src/main/java/com/example/restfuldemo/RestfuldemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.restfuldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfuldemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestfuldemoApplication.class, args);
+    }
+
+}
