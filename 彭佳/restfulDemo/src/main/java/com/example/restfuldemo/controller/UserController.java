a7f861e3014cb34110b68b180b6e007d20f390f7
彭佳 <486904116@qq.cm>
2024-04-01 22:34:42 +0800

    Revert "彭佳的第三次提交"
    This reverts commit fc4ebbc818ce6819d36b784afc8d361344fa6b99.

--- 彭佳/restfulDemo/src/main/java/com/example/restfuldemo/controller/UserController.java
+++ 彭佳/restfulDemo/src/main/java/com/example/restfuldemo/controller/UserController.java
@@ -3,7 +3,8 @@ package com.example.restfuldemo.controller;
 import com.example.restfuldemo.model.User;
 import com.example.restfuldemo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -14,36 +15,10 @@ public class UserController {
     UserService userService;
 
     @GetMapping("/getAllUsers")
-    public List<User> getAllUsers() {
-        List<User> userList = userService.list();
+    public List<User> getAllUsers()
+    {
+        List<user> userList = userService.list();
         return userList;
-    }
-
-    @GetMapping("/user/{id}")
-    public User getUserById(@PathVariable("id") Integer id){
-        User user = userService.getById(id);
-        return  user;
-    }
-    @PostMapping("/user")
-    public String addUser(@RequestBody User user){
-        if(userService.save(user)){
-            return "添加用户成功";
-        }
-        else  return "添加用户失败";
-    }
-    @PutMapping("/user")
-    public String updataUser(@RequestBody User user){
-        if (userService.updateById(user)){
-            return "修改用户成功";
-        }
-        else  return "修改用户失败";
-
-    }
-    public String deleteUser(Integer id){
-        if(userService.removeById(id)){
-            return "删除用户成功";
-        }
-        else return "删除用户失败";
     }
 }
 
--- 彭佳/restfulDemo/src/main/java/com/example/restfuldemo/service/impl/UserServiceImpl.java
+++ 彭佳/restfulDemo/src/main/java/com/example/restfuldemo/service/impl/UserServiceImpl.java
@@ -1,28 +1,10 @@
 package com.example.restfuldemo.service.impl;
 
-import com.baomidou.mybatisplus.core.conditions.Wrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.example.restfuldemo.mapper.UserMapper;
-import com.example.restfuldemo.model.User;
 import com.example.restfuldemo.service.UserService;
 import org.springframework.stereotype.Service;
 
-import java.util.function.Function;
-
 @Service
-public class UserServiceImpl extends ServiceImpl<UserMapper,User> implements UserService {
-    @Override
-    public boolean save(User entity) {
-        return super.save(entity);
-    }
-
-    @Override
-    public boolean saveOrUpdate(User entity) {
-        return false;
-    }
-
-//    @Override
-//    public Object (Wrapper<User> queryWrapper, Function<? super Object, > mapper) {
-//        return null;
- //   }
+public class UserServiceImpl extends ServiceImpl<UserMapper> implements UserService {
 }
