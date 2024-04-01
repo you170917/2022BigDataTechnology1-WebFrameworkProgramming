1fe206c23de91dd9356bb7f2818bfc46e492f1c2
username <2675125430@qq.com>
2024-04-01 10:40:53 +0800

    罗成龙第三次提交作业

--- 罗成龙/homework03/src/main/java/com/example/homework03/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.example.homework03.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.example.homework03.bean.User;
-
-public interface UserService extends IService<User> {
-}
