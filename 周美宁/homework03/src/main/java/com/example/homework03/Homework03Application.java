b6c3a6335f0b63f3efbdd7f14c4c253934cecd03
ZhouMeiNing <2893039946@qq.com>
2024-04-01 16:41:19 +0800

    周美宁的第三次提交

--- 周美宁/homework03/src/main/java/com/example/homework03/Homework03Application.java
+++ 周美宁/homework03/src/main/java/com/example/homework03/Homework03Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Homework03Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Homework03Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Homework03Application.class, args);
+    }
 
 }
--- 周美宁/homework03/src/test/java/com/example/homework03/Homework03ApplicationTests.java
+++ 周美宁/homework03/src/test/java/com/example/homework03/Homework03ApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class Homework03ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
