e608d243d1cec0029e2ac201fc45aee8e107a3e0
= <=>
2024-04-03 10:18:39 +0800

    郭雪馨的第三、四ã、五次提交

---
+++ 郭雪馨/homework05/src/main/java/com/example/homework05/model/User.java
@@ -0,0 +1,13 @@
+package com.example.homework05.model;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+
+@TableName("user")
+public class User {
+    @TableId(value = "id",type = IdType.AUTO)
+    private Integer id;
+    private String username;
+    private String address;
+}
