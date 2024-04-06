63ac146bac2b22d3a98d0f1656d5eae549fdc201
XiaoQing <2984754179@qq.com>
2024-04-06 16:09:33 +0800

    Revert "周晓清第五次提交"
    This reverts commit 876890826c01a50a5efdb5f7400a26055063ac56.

--- 周晓清/Mybatisplusdemo01/src/main/java/com/example/mybatisplusdemo01/mapper/UserMapper.java
+++
@@ -1,8 +0,0 @@
-package com.example.mybatisplusdemo01.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.example.mybatisplusdemo01.dean.User;
-
-public interface UserMapper extends BaseMapper<User> {
-
-}
--- 周晓清/Mybatisplusdemo01/src/main/java/com/example/mybatisplusdemo01/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.example.mybatisplusdemo01.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.example.mybatisplusdemo01.dean.User;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface UserService extends IService<User> {
-}
