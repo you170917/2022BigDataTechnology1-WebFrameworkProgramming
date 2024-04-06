8056f6a4354bcd65e97c777a483b9b82cdf852ef
XiaoQing <2984754179@qq.com>
2024-04-06 16:07:59 +0800

    Revert "周晓清第五次提交"
    This reverts commit b40fffd55b16e3f87c3b40c94e9b75d52cc90982.

--- 周晓清/restfulDemo/src/main/java/com/example/restfuldemo/mapper/UserMapper.java
+++
@@ -1,7 +0,0 @@
-package com.example.restfuldemo.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.example.restfuldemo.demos.web.User;
-
-public interface UserMapper extends BaseMapper<User> {
-}
