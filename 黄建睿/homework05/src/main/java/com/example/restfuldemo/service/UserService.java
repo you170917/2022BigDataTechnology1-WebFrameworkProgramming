f595b352e9fa214abb5f78a599c5e3350b0caefe
=hxwzhenshuai <=2425683342@qq.com>
2024-04-09 13:37:37 +0800

    黄建睿的第五次提交

---
+++ 黄建睿/homework05/src/main/java/com/example/restfuldemo/service/UserService.java
@@ -0,0 +1,8 @@
+package com.example.restfuldemo.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.example.restfuldemo.model.User;
+
+
+public interface UserService extends IService<User> {
+}
