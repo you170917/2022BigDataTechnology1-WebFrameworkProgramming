3ae25668fe04286492f44763d6ec9e599db36446
问问 <1304163290@qq.com>
2024-04-21 23:39:12 +0800

    王雪婷的第七次提交

--- 王雪婷/homework05/src/main/java/com/example/homework05/model/User.java
+++ 王雪婷/homework05/src/main/java/com/example/homework05/model/User.java
@@ -1,7 +1,6 @@
 package com.example.homework05.model;
 
 import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
@@ -11,8 +10,6 @@ import lombok.Data;
 public class User {
     @TableId(value = "id", type = IdType.AUTO)
     private Integer id;
-    @TableField("user")
-    private String username;
+    private String user;
     private String address;
 }
-
--- 王雪婷/homework05/src/main/java/com/example/homework05/service/UserService.java
+++ 王雪婷/homework05/src/main/java/com/example/homework05/service/UserService.java
@@ -3,5 +3,5 @@ package com.example.homework05.service;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.example.homework05.model.User;
 
-public interface UserService  extends IService<User> {
+public interface UserService extends IService<User> {
 }
