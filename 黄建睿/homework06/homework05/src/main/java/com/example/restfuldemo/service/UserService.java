9c45a0a26d146b633926a5d0a2250e4ac8b0dfe4
hxwzhenshuai <162101942+hxwzhenshuai@users.noreply.github.com>
2024-04-25 13:40:43 +0800

    Delete 黄建睿/homework06 directory

--- 黄建睿/homework06/homework05/src/main/java/com/example/restfuldemo/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.example.restfuldemo.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.example.restfuldemo.model.User;
-
-
-public interface UserService extends IService<User> {
-}
