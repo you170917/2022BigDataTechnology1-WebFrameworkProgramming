36b8e16d22606d7423400bee334af3551f70a20f
=hxwzhenshuai <=2425683342@qq.com>
2024-04-25 13:20:49 +0800

    黄建睿的第五次提交

--- 黄建睿/homework05/src/main/java/com/example/restfuldemo/Config/webMvcConfig.java
+++ 黄建睿/homework05/src/main/java/com/example/restfuldemo/Config/webMvcConfig.java
@@ -1,11 +1,8 @@
 package com.example.restfuldemo.Config;
 @Configuration
 public class WebMvcConfig  implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080")
-                .allowedMethods("*")
-                .allowedHeaders("*");
+
+    public void addCorsMappings() {
+
     }
 }
