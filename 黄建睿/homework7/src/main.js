43282795fb9abe0826f7379604d0a0b2761d62eb
=hxwzhenshuai <=2425683342@qq.com>
2024-05-06 14:50:17 +0800

    黄建睿的第八次提交

--- 黄建睿/homework7/src/main.js
+++ 黄建睿/homework7/src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import {deleteRequest, getRequest, postRequest, putRequest} from "@/utils/api";
+import router from "@/router";
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
+// 通过 Vue.prototype 注册全局变量,通过 this.getRequest 就可以调用该方法
+Vue.prototype.getRequest = getRequest;
+Vue.prototype.postRequest = postRequest;
+Vue.prototype.putRequest = putRequest;
+Vue.prototype.deleteRequest = deleteRequest;
 Vue.config.productionTip = false
 
+
 new Vue({
-  render: h => h(App),router,
+  render: h => h(App),router
 }).$mount('#app')
--- 黄建睿/homework7/src/views/User.vue
+++ 黄建睿/homework7/src/views/User.vue
@@ -18,100 +18,99 @@
       请输入要添加的用户信息：
       <div>
         username
-        <input v-model="username">
+        <input v-model="addUserParams.username">
       </div>
       <div>
         address
-        <input v-model="address">
+        <input v-model="addUserParams.address">
       </div>
     </div>
     <button v-on:click="addUser">添加用户</button>
-    <p></p>
     <div>
       请输入要修改的用户信息：
       <div>
         id
-        <input v-model="id">
+        <input v-model="updateUserParams.id">
       </div>
       <div>
         username
-        <input v-model="username">
+        <input v-model="updateUserParams.username">
       </div>
       <div>
         address
-        <input v-model="address">
+        <input v-model="updateUserParams.address">
       </div>
     </div>
-    <button v-on:click="updateUser">修改用户</button>
-    <p></p>
-    <div>
-      请输入删除用户的id：
-      <input v-model = "deleteId">
-      <div>
-        <button v-on:click="deleteUser">删除用户</button>
-      </div>
-    </div>
-  </div>
+    <button v-on:click="updateUser">
+      修改用户
+    </button>
 </template>
 <script>
-import axios from "axios";
+import address from "address";
+
 export default {
   name: "UserPage",
   data(){
-    return{
-      users: null,
-      queryId: null,
-      userById: null,
-      username: null,
-      address: null,
-      id: null,
-      deleteId: null
-    }
+        return{
+          addUserParams: {
+            username: null,
+            address: null,
+          },
+          updateUserParams: {
+            id: null,
+            username: null,
+            address: null,
+          }
+        }
   },
   methods: {
-    getAllUsers(){
-      axios.get("http://localhost:8081/AllUsers")
-          .then(resp => {
-            console.log(resp);
-            this.users = resp.data;
-          })
+    address,
+    getAllUsers() {
+      this.getRequest("/AllUsers").then(resp => {
+        console.log(resp);
+        if (resp.status == 200) {
+          this.users = resp.data.data; //通过 resp.data.data 拿到返回数据
+        } else {
+          alert(resp.data.msg);
+        }
+      })
     },
-    getUserById(){
-      axios.get(`http://localhost:8081/user/${this.queryId}`)
-          .then(resp => {
-            console.log(resp);
-            this.userById = resp.data;
-          })
+    getUserById() {
+      this.getRequest(`/user/${this.queryId}`).then(resp => {
+        console.log(resp);
+        if (resp.status == 200) {
+                this.userById = resp.data.data;
+        } else {
+          alert(resp.data.msg);
+        }
+      })
     },
     addUser(){
-      axios.post("http://localhost:8081/user",
-          {
-            username: this.username,
-            address: this.address
-          }).then(resp => {
+      this.postRequest("/user", {
+        username: this.addUserParams.username,
+        address: this.addUserParams.address
+      }).then(resp => {
         console.log(resp);
       })
     },
     updateUser(){
-      axios.put("http://localhost:8081/user",{
-        id: this.id,
-        username: this.username,
-        address: this.address
-      }).then( resp => {
+      this.putRequest("/user", {
+        id: this.updateUserParams.id,
+        username: this.updateUserParams.username,
+        address: this.updateUserParams.address
+      }).then(resp => {
         console.log(resp);
       })
+    }
     },
-    deleteUser(){
-      axios.delete("http://localhost:8081/user", {
-        params: {
-          id: this.deleteId
-        }
+    deleteUser() {
+      this.deleteRequest("/user", {
+        id: this.deleteId
       }).then(resp => {
         console.log(resp);
       })
     }
   }
-}
 </script>
 <style scoped>
 </style>
