4332d96dc0e4f5b68927f69d6a7199c8b36cdc57
= <=>
2024-05-06 15:30:46 +0800

    郭雪馨的第九次提交

---
+++ 郭雪馨/homework09/src/main/java/com/example/homework09/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.homework09.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class HelloController {
+    @GetMapping("/admin/hello")
+    public String hello1(){
+        return "hello admin~";
+    }
+    @GetMapping("/user/hello")
+    public String hello2(){
+        return "hello user!";
+    }
+}
