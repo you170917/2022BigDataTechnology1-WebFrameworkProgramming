513b436ac22de74f57c7452f0984cd3f4936d670
= <=>
2024-05-15 14:57:20 +0800

    李佳洁的第五次提交

---
+++ 李佳洁/homework5/src/main/java/com/example/homework5/service/impl/UserServiceImpl.java
@@ -0,0 +1,11 @@
+package com.example.homework5.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.example.homework5.mapper.UserMapper;
+import com.example.homework5.model.User;
+import com.example.homework5.service.UserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements UserService {
+}
