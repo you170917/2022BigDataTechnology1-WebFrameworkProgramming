3bd51006b4d4e74fb2ff5b479d475572ac67d66d
= <=>
2024-05-15 14:15:05 +0800

    李佳洁的第十次提交（第二次提交）

---
+++ 李佳洁/homework10_back01/src/main/java/com/example/homework10_back01/service/UserService.java
@@ -0,0 +1,7 @@
+package com.example.homework10_back01.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.example.homework10_back01.model.User;
+
+public interface UserService extends IService<User> {
+}
