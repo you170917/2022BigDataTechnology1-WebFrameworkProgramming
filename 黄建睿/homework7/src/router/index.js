3817dab874540cca81b282556c013164acafb62f
=hxwzhenshuai <=2425683342@qq.com>
2024-05-20 11:58:42 +0800

    黄建睿的第九，十次提交

--- 黄建睿/homework7/src/router/index.js
+++ 黄建睿/homework7/src/router/index.js
@@ -2,9 +2,12 @@ import Vue from "vue";  //导入 Vue 模块
 import VueRouter from "vue-router";
 import User from "@/views/User.vue";
 import NotFound from "@/views/NotFound.vue";
-import MyUser from "@/views/MyUser.vue"; // 第二步：导入 VueRouter 模块
+import MyUser from "@/views/MyUser.vue";
+import Home from "@/views/Home.vue";
+import Login from "@/views/Login.vue"; // 第二步：导入 VueRouter 模块
 Vue.use(VueRouter); //第三步：Vue.use 安装注册
 const routes = [
+
     {
         path:"/",
         redirect:"/user"
@@ -21,7 +24,16 @@ const routes = [
         path:"/MyUser",
         component: MyUser
     },
-]
+
+    {
+        path: "/home",
+        component: Home
+    },
+    {
+        path: "/",
+        component: Login
+    }
+    ]
 const router =new VueRouter({
     router: routes
 });
