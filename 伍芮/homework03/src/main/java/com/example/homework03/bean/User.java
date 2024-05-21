95c2d03974017b2ecea9714e56d28239564b43cc
伍芮 <1984342522@.com>
2024-05-21 13:18:11 +0800

    伍芮的第3次提交

---
+++ 伍芮/homework03/src/main/java/com/example/homework03/bean/User.java
@@ -0,0 +1,8 @@
+package com.example.homework03.bean;
+
+@Data
+public class User {
+    private Integer id;
+    private String username;
+    private String address;
+}
---
+++ 伍芮/homework03/src/main/java/com/example/homework03/service/impl/UserServiceImpl.java
@@ -0,0 +1,6 @@
+package com.example.homework03.service.impl;
+
+@Service
+public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements
+        UserService{
+}
