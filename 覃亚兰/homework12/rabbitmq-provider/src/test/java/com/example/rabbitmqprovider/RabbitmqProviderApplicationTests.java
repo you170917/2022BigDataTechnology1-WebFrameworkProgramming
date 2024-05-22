379dbaccd1b555a5bf353f7cbd03ccadd7608f86
qinyalan <xialan3315694298@qq.com>
2024-05-22 09:54:02 +0800

    覃亚兰的第十二次作业

---
+++ 覃亚兰/homework12/rabbitmq-provider/src/test/java/com/example/rabbitmqprovider/RabbitmqProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rabbitmqprovider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqProviderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
