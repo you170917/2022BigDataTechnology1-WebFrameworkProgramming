4b0989fe6d5d27e05737a650883ea36c6d5dd3dd
= <=>
2024-05-22 11:08:55 +0800

    李佳洁的第十二次提交

---
+++ 李佳洁/homework12/rabbitmq-provider/src/main/java/com/example/rabbitmqprovider/config/RabbitConfig.java
@@ -0,0 +1,26 @@
+package com.example.rabbitmqprovider.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitConfig {
+    @Bean
+    public Queue queue(){
+        return new Queue("test");
+    }
+    @Bean
+    DirectExchange exchange() {
+        return new DirectExchange("exchange");
+    }
+    //绑定 将队列和交换机绑定, 并设置用于匹配键：TestDirectRouting
+    @Bean
+    Binding bindingDirect() {
+        return
+                BindingBuilder.bind(queue()).to(exchange()).with("TestDirectRouting");
+    }
+}
