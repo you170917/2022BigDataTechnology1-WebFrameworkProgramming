0ac7e95ac1e5fb4e068c45b71c9aba00b5b9da18
xi <2812527373@qq.com>
2024-05-27 10:15:46 +0800

    陈颃的第十一次提交

--- 陈颃/homework08/03.1/src/utils/api.js
+++ 陈颃/homework08/03.1/src/utils/api.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+axios.defaults.withCredentials = true;
 
 let baseurl = "http://localhost:8082";
 //GET
--- 陈颃/homework08/03.2/src/main/java/com/example/demo/config/WebMvcConfig.java
+++ 陈颃/homework08/03.2/src/main/java/com/example/demo/config/WebMvcConfig.java
@@ -8,13 +8,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")
-//                .allowedOrigins("http://localhost:8080")
-//                .allowedMethods("*")
-//                .allowedHeaders("*");
-//    }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080")
+                .allowedMethods("*")
+                .allowedHeaders("*");
+                .allowCredentials(true);
+    }
 
     @Bean
     public WebMvcConfigurer corsConfigurer()
