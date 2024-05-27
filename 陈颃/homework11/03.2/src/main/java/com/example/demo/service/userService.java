9ecff00c322d812376092af77017c948533243de
xi <2812527373@qq.com>
2024-05-27 10:15:46 +0800

    陈颃的第十一次提交

--- 陈颃/homework11/03.2/src/main/java/com/example/demo/service/userService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.example.demo.bean.User;
-import org.springframework.stereotype.Service;
-
-
-public interface userService extends IService<User>{
-}
