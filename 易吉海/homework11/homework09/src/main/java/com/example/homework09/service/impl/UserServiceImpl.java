7f07ed43ecd7a1780cc64ea9f2c39877799a528c
Hai0217 <162075435+Hai0217@users.noreply.github.com>
2024-06-12 11:54:47 +0800

    易吉海的第十一次提交

---
+++ 易吉海/homework11/homework09/src/main/java/com/example/homework09/service/impl/UserServiceImpl.java
@@ -0,0 +1,11 @@
+package com.example.homework09.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.example.homework09.mapper.UserMapper;
+import com.example.homework09.model.User;
+import com.example.homework09.service.UserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl extends ServiceImpl<UserMapper , User> implements UserService {
+}
