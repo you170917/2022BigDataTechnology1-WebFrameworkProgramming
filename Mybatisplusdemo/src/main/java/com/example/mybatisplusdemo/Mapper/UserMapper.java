5d28d1518789b9eb7f1990d95101eb93707394f4
Hai0217 <162075435+Hai0217@users.noreply.github.com>
2024-06-12 08:31:32 +0800

    Delete Mybatisplusdemo directory

--- Mybatisplusdemo/src/main/java/com/example/mybatisplusdemo/Mapper/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package com.example.mybatisplusdemo.Mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.example.mybatisplusdemo.bean.User;
-
-public interface UserMapper extends BaseMapper<User> {
-    }
-
-
