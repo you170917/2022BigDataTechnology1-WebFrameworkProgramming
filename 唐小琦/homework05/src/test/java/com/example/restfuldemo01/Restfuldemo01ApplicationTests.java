368d5c9e0410dc942dd7b1f2d964c843c6bcffee
tangxiaoqi <2010147314@qq.com>
2024-06-19 10:38:01 +0800

    唐小琦的第五、六、七、八、九、十、十一、十二次提交

---
+++ 唐小琦/homework05/src/test/java/com/example/restfuldemo01/Restfuldemo01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restfuldemo01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Restfuldemo01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
