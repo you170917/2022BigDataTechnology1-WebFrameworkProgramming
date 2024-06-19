02a1d2b46508f4b456a1aa0ba821db64bb4916fb
彭佳 <486904116@qq.cm>
2024-06-19 11:21:54 +0800

    彭佳的第八、九、十、十一、十二次提交

---
+++ 彭佳/homework12/src/main/java/com/example/homework12/Homework12Application.java
@@ -0,0 +1,13 @@
+package com.example.homework12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework12Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Homework12Application.class, args);
+    }
+
+}
