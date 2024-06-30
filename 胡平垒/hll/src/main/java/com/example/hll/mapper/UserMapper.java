2c05c69496d0ec02661d0b25958472d21a5aa494
origin <git@github.com:you170917/2022BigDataTechnology1-WebFrameworkProgramming.git>
2024-06-30 16:49:03 +0800

    胡平垒的第九次提交

---
+++ 胡平垒/hll/src/main/java/com/example/hll/mapper/UserMapper.java
@@ -0,0 +1,7 @@
+package com.example.hll.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.hll.model.User;
+
+public interface UserMapper extends BaseMapper<User> {
+}
