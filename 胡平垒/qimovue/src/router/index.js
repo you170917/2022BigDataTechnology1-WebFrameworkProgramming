095355d6cfb0dd973c7c4e12259882ba1b47ace0
origin <git@github.com:you170917/2022BigDataTechnology1-WebFrameworkProgramming.git>
2024-06-30 16:30:00 +0800

    胡平垒的第八次提交

---
+++ 胡平垒/qimovue/src/router/index.js
@@ -0,0 +1,6 @@
+import Vue from "vue"; //导入 Vue 模块
+import VueRouter from "vue-router"; // 第二步：导入 VueRouter 模块
+Vue.use(VueRouter); //第三步：Vue.use 安装注册
+const router = new VueRouter({ //第四步：创建 VueRouter 对象
+});
+export default router;
